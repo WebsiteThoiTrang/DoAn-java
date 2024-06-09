9bdb44c0c53b36d0eb9f55f9168dbb02fc81b27e
nguyenchinhtan2003@gmail.com <nguyenchinhtan2003@gmail.com>
2024-06-09 10:59:12 +0700

    init project

---
+++ src/main/java/com/nhom8/WebsiteThoiTrang/WebsiteThoiTrangApplication.java
@@ -0,0 +1,13 @@
+package com.nhom8.WebsiteThoiTrang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsiteThoiTrangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsiteThoiTrangApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nhom8/WebsiteThoiTrang/WebsiteThoiTrangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhom8.WebsiteThoiTrang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsiteThoiTrangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
