22ab474b3c8fcc71e264aac0f075b7ca412b0128
triethaha71 <triethaha71@gmail.com>
2024-06-25 14:19:02 +0700

    v

--- src/main/java/com/nhom8/WebsiteThoiTrang/controller/admin/AdminController.java
+++ src/main/java/com/nhom8/WebsiteThoiTrang/controller/admin/AdminController.java
@@ -1,12 +1,19 @@
 package com.nhom8.WebsiteThoiTrang.controller.admin;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+@RequestMapping("/admin")
 @Controller
 public class AdminController {
-    @RequestMapping("/admin")
+    @GetMapping
     public String admin() {
         return "admin/index";
     }
+
+        @GetMapping("/admincontact")
+        public String AdminContact() {
+            return "admin/admincontact/index";
+        }
+
 }
