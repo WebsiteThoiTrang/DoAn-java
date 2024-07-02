94defa639fbe57a830d3f3825de8ff6edca03a12
triethaha71 <triethaha71@gmail.com>
2024-07-02 09:38:07 +0700

    Product detail

--- src/main/java/com/nhom8/WebsiteThoiTrang/controller/user/ProductController.java
+++ src/main/java/com/nhom8/WebsiteThoiTrang/controller/user/ProductController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -23,5 +24,20 @@ public class ProductController {
         List<Product> getAllProducts = productService.getAllProducts();
         model.addAttribute("products",getAllProducts);
         return "/user/product/index";
+    }
+
+//    @GetMapping("/detail")
+//    public String detail() {
+//
+//        return "/user/product/detail";
+//    }
+
+    @GetMapping("/detail")
+    public String showProductDetail(@RequestParam("id") Long id, Model model) {
+        // Retrieve product details from the service based on productId
+        Product product = productService.getProductById(id);
+        // Add product object to the model to be accessed by Thymeleaf template
+        model.addAttribute("product", product);
+        return "/user/product/detail"; // Returns the name of the Thymeleaf template (productDetail.html)
     }
 }
